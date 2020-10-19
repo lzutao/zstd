0e7c168b8510f68aa508443485b3485929e8744d
Lzu Tao <taolzu@gmail.com>
2020-10-19 10:35:50 +0700

    visualbility

--- lib/compress/zstd_compress.c
+++ lib/compress/zstd_compress.c
@@ -1057,7 +1057,7 @@ ZSTD_clampCParams(ZSTD_compressionParameters cParams)
 
 /** ZSTD_cycleLog() :
  *  condition for correct operation : hashLog > 1 */
-ZDICTLIB_API U32 ZSTD_cycleLog(U32 hashLog, ZSTD_strategy strat)
+ZSTDLIB_API U32 ZSTD_cycleLog(U32 hashLog, ZSTD_strategy strat)
 {
     U32 const btScale = ((U32)strat >= (U32)ZSTD_btlazy2);
     return hashLog - btScale;
